d756d3e1d20c3e84b9498ceba052f23f8f9c0220
Mohamed Charfeddine <110181648+medcharfeddine@users.noreply.github.com>
2023-09-18 10:41:18 +0100

    Update private.client.js

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://movie-app-project-api.vercel.app/api/v1/";
+const baseURL = "https://movie-app-api-theta.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
